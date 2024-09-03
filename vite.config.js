6ff192cf14c05459492f50c55f685fdf913013ac
Matiasrod132 <matiasrodriesc@gmail.com>
2024-09-03 14:25:29 -0500

    final

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Cambia 'build' por el nombre de la carpeta que prefieras
+  },
 })
