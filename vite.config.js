ae2019795b34fcbb5831c83a00caafe842d8b344
Matiasrod132 <matiasrodriesc@gmail.com>
2024-09-03 14:32:37 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Cambia 'build' por el nombre de la carpeta que prefieras
-  },
-})
+    outDir: 'dist' // Asegúrate de que 'dist' sea la carpeta de salida
+  }
+});
